3def53b25adee098739cd4f5e0839280cd3decca
Saul Lee <solleedata@gmail.com>
2023-10-27 00:01:06 +0900

    feat: Loading spinner component

---
+++ pages/components/Loading.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  display: block;
+  margin: 1rem auto;
+  position: relative;
+  background: #fff;
+  box-shadow: -1.5rem 0 #fff, 1.5rem 0 #fff;
+  box-sizing: border-box;
+  animation: shadowPulse 2s linear infinite;
+
+  @keyframes shadowPulse {
+    33% {
+      background: #fff;
+      box-shadow: -1.5rem 0 #999, 1.5rem 0 #fff;
+    }
+    66% {
+      background: #999;
+      box-shadow: -1.5rem 0 #fff, 1.5rem 0 #fff;
+    }
+    100% {
+      background: #fff;
+      box-shadow: -1.5rem 0 #fff, 1.5rem 0 #999;
+    }
+  }
+`;
