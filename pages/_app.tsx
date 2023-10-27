df32247af31b998c6e6aa5b0bbe1458f654d449c
Saul Lee <solleedata@gmail.com>
2023-10-27 11:44:27 +0900

    fix: delete ;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
