e8414ddb239c7156e019bce383ab5d238e4da157
Saul Lee <solleedata@gmail.com>
2024-02-12 11:24:21 +0900

    feat: 시스템 preferences에 따란 라이트/다크 모드 자동으로 설정하기 #14

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import './globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
+    <ThemeProvider attribute="class">
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
